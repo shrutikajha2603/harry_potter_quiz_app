d1a32c67caa833cd5ce02f5e192a700d2bedeff5
Shrutika Jha <shrutikajha2603@gmail.com>
2024-03-17 16:04:37 +0530

    quizzz

---
+++ src/assets/data.js
@@ -0,0 +1,27 @@
+export const data = [
+    {
+        question: "Who was trying to make Harry Potter fall from his broom during Quiditch Match in Chambers of Secret?",
+        option1: "Prof. Dumbledore",
+        option2: "Draco Malfoy",
+        option3: "Prof. Quirrel",
+        option4: "Prof. Snape",
+        ans: 3,
+    },
+    {
+        question: "Who removes all the bones from Harry’s arm?",
+        option1: "Gilderoy Lockhart",
+        option2: "Hagrid",
+        option3: "Voldemort",
+        option4: "Neville Longbottom",
+        ans: 1,
+    },
+    {
+        question: "How many Horcruxes did Voldemort create intentionally?",
+        option1: 5,
+        option2: 6,
+        option3: 7,
+        option4: 'none',
+        ans: 3,
+    },
+
+];
